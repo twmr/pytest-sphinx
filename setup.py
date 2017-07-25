cfa7a9b5efd4c1625db263a4de26d87768453ddc
Thomas Hisch <t.hisch@gmail.com>
2017-07-25 20:30:06 +0200

    initial commit
    Signed-off-by: Thomas Hisch <t.hisch@gmail.com>

---
+++ setup.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import codecs
+from setuptools import setup
+
+
+def read(fname):
+    file_path = os.path.join(os.path.dirname(__file__), fname)
+    return codecs.open(file_path, encoding='utf-8').read()
+
+
+setup(
+    name='pytest-sphinx',
+    version='0.1.0',
+    author='Thomas Hisch',
+    author_email='t.hisch@gmail.com',
+    maintainer='Thomas Hisch',
+    maintainer_email='t.hisch@gmail.com',
+    license='BSD-3',
+    url='https://github.com/thisch/pytest-sphinx',
+    description='A simple plugin to use with Pytest',
+    long_description=read('README.rst'),
+    py_modules=['pytest_sphinx'],
+    install_requires=['pytest>=3.1.1'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Framework :: Pytest',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Testing',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+        'Operating System :: OS Independent',
+        'License :: OSI Approved :: BSD License',
+    ],
+    entry_points={
+        'pytest11': [
+            'sphinx = pytest_sphinx',
+        ],
+    },
+)
