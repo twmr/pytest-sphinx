5af74551499b4c5f34a35355a0bb06a411181ee7
Thomas Hisch <t.hisch@gmail.com>
2017-10-30 11:35:08 +0000

    Remove usage of izip, which is not available in py3

--- pytest_sphinx.py
+++ pytest_sphinx.py
@@ -32,7 +32,7 @@ def pairwise(iterable):
     "s -> (s0,s1), (s1,s2), (s2, s3), ..."
     a, b = itertools.tee(iterable)
     next(b, None)
-    return itertools.izip(a, b)
+    return list(zip(a, b))
 
 
 class DoctestDirectives(enum.Enum):
