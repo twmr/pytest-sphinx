c0e3c27670585f48891b13f974ca9527c4afda61
Thomas Hisch <t.hisch@gmail.com>
2017-11-04 12:14:19 +0100

    Add CHANGELOG file

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ def read(fname):
 
 setup(
     name='pytest-sphinx',
-    version='0.1.1',
+    version='0.1.2',
     author='Thomas Hisch',
     author_email='t.hisch@gmail.com',
     maintainer='Thomas Hisch',
