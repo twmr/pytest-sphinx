ab314ca1aa81d28baf733b6c927317e3f3cee3c3
Thomas Hisch <t.hisch@gmail.com>
2019-02-24 20:15:04 +0100

    Add support for indented directives
    Closes: #6

--- tests/test_doc2test.py
+++ tests/test_doc2test.py
@@ -1,4 +1,5 @@
 import doctest
+import textwrap
 
 from pytest_sphinx import docstring2examples
 
@@ -49,3 +50,26 @@ def test_with_options():
     assert example.options == {
         doctest.NORMALIZE_WHITESPACE: True,
         doctest.ELLIPSIS: True}
+
+
+def test_indented():
+    doc = textwrap.dedent("""
+    Examples:
+        some text
+
+        .. testcode::
+
+            print("Banana")
+
+        .. testoutput::
+
+            Banana
+    """)
+
+    examples = docstring2examples(doc)
+    assert len(examples) == 1
+    example = examples[0]
+
+    assert example.want == "Banana\n"
+    assert example.exc_msg is None
+    assert example.options == {}
