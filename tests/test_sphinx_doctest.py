941fb1a495165840c75c9c0e2860d374fc153447
Tony Narlock <tony@git-pull.com>
2022-09-07 19:22:11 -0500

    refactor: Strict typings

--- tests/test_sphinx_doctest.py
+++ tests/test_sphinx_doctest.py
@@ -3,15 +3,20 @@ import logging
 import os
 import subprocess
 import textwrap
+from pathlib import Path
+from typing import Iterator
+from typing import Union
 
 import pytest
+from _pytest.legacypath import Testdir
+from py._path.local import LocalPath
 
 logger = logging.getLogger(__name__)
 
 
 class SphinxDoctestRunner:
-    def __init__(self, tmpdir):
-        self.tmpdir = tmpdir
+    def __init__(self, tmp_path: Path) -> None:
+        self.tmp_path: Path = tmp_path
         subprocess.check_output(
             [
                 "sphinx-quickstart",
@@ -31,10 +36,12 @@ class SphinxDoctestRunner:
             ]
         )
 
-    def __call__(self, rst_file_content, must_raise=False, sphinxopts=None):
-        index_rst = self.tmpdir.join("source").join("index.rst")
+    def __call__(
+        self, rst_file_content: str, must_raise: bool = False, sphinxopts: None = None
+    ) -> str:
+        index_rst = self.tmp_path / "source" / "index.rst"
         rst_file_content = textwrap.dedent(rst_file_content)
-        index_rst.write(rst_file_content)
+        index_rst.write_text(rst_file_content, encoding="utf-8")
         logger.info("CWD: %s", os.getcwd())
         logger.info("content of index.rst:\n%s", rst_file_content)
 
@@ -42,8 +49,11 @@ class SphinxDoctestRunner:
         if sphinxopts:
             cmd.append(sphinxopts)
 
-        def to_str(subprocess_output):
-            output_str = "\n".join(subprocess_output.decode().splitlines())
+        def to_str(subprocess_output: Union[str, bytes]) -> str:
+            if isinstance(subprocess_output, bytes):
+                output_str = "\n".join(subprocess_output.decode().splitlines())
+            else:
+                output_str = subprocess_output
             logger.info("%s produced:\n%s", cmd, output_str)
             return output_str
 
@@ -55,12 +65,12 @@ class SphinxDoctestRunner:
 
 
 @pytest.fixture
-def sphinx_tester(tmpdir):
+def sphinx_tester(tmpdir: LocalPath) -> Iterator[SphinxDoctestRunner]:
     with tmpdir.as_cwd():
         yield SphinxDoctestRunner(tmpdir)
 
 
-def test_simple_doctest_failure(sphinx_tester):
+def test_simple_doctest_failure(sphinx_tester: SphinxDoctestRunner) -> None:
 
     output = sphinx_tester(
         """
@@ -86,7 +96,7 @@ def test_simple_doctest_failure(sphinx_tester):
     assert expected in output, f"{expected!r}\n\n{output!r}"
 
 
-def test_simple_doctest_success(sphinx_tester):
+def test_simple_doctest_success(sphinx_tester: SphinxDoctestRunner) -> None:
     output = sphinx_tester(
         """
         ===!!!
@@ -99,7 +109,9 @@ def test_simple_doctest_success(sphinx_tester):
 
 
 class TestDirectives:
-    def test_testcode(self, testdir, sphinx_tester):
+    def test_testcode(
+        self, testdir: Testdir, sphinx_tester: SphinxDoctestRunner
+    ) -> None:
         code = """
             .. testcode::
 
@@ -116,7 +128,9 @@ class TestDirectives:
         plugin_result = testdir.runpytest("--doctest-glob=index.rst").stdout
         plugin_result.fnmatch_lines(["*=== 1 passed in *"])
 
-    def test_doctest(self, testdir, sphinx_tester):
+    def test_doctest(
+        self, testdir: Testdir, sphinx_tester: SphinxDoctestRunner
+    ) -> None:
         code = """
             .. doctest::
 
@@ -130,7 +144,9 @@ class TestDirectives:
         plugin_result = testdir.runpytest("--doctest-glob=index.rst").stdout
         plugin_result.fnmatch_lines(["*=== 1 passed in *"])
 
-    def test_doctest_multiple(self, testdir, sphinx_tester):
+    def test_doctest_multiple(
+        self, testdir: Testdir, sphinx_tester: SphinxDoctestRunner
+    ) -> None:
         code = """
             .. doctest::
 
@@ -160,7 +176,9 @@ class TestDirectives:
         plugin_result.fnmatch_lines(["*=== 1 passed in *"])
 
     @pytest.mark.parametrize("testcode", ["raise RuntimeError", "pass", "print(1234)"])
-    def test_skipif_true(self, testdir, sphinx_tester, testcode):
+    def test_skipif_true(
+        self, testdir: Testdir, sphinx_tester: SphinxDoctestRunner, testcode: str
+    ) -> None:
         code = """
             .. testcode::
 
@@ -191,7 +209,9 @@ class TestDirectives:
     @pytest.mark.parametrize(
         "testcode", ["raise RuntimeError", "pass", "print('EVALUATED')"]
     )
-    def test_skipif_false(self, testdir, sphinx_tester, testcode):
+    def test_skipif_false(
+        self, testdir: Testdir, sphinx_tester: SphinxDoctestRunner, testcode: str
+    ) -> None:
         code = """
             .. testcode::
 
@@ -219,8 +239,11 @@ class TestDirectives:
 
     @pytest.mark.parametrize("wrong_output_assertion", [True, False])
     def test_skipif_multiple_testoutput(
-        self, testdir, sphinx_tester, wrong_output_assertion
-    ):
+        self,
+        testdir: Testdir,
+        sphinx_tester: SphinxDoctestRunner,
+        wrong_output_assertion: bool,
+    ) -> None:
         # TODO add test, where there are muliple un-skipped testoutput
         # sections. IMO this must lead to a testfailure, which is currently
         # not the case in sphinx -> Create sphinx ticket
@@ -259,7 +282,9 @@ class TestDirectives:
             plugin_output.fnmatch_lines(["*=== 1 passed in *"])
 
     @pytest.mark.parametrize("testcode", ["raise RuntimeError", "pass", "print(1234)"])
-    def test_skipif_true_in_testcode(self, testdir, sphinx_tester, testcode):
+    def test_skipif_true_in_testcode(
+        self, testdir: Testdir, sphinx_tester: SphinxDoctestRunner, testcode: str
+    ) -> None:
         code = """
             .. testcode::
                 :skipif: True
